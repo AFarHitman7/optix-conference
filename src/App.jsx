32c0bd835258adb1348bd9bde4ec494bf43130e2
AFarHitman7 <rafanhatim@gmail.com>
2026-02-15 02:11:26 +0530

    vercel fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
-import Success from "./pages/Success";
 import AboutPage from "./pages/AboutPage";
 import Abstract from "./pages/Abstract";
 
@@ -11,7 +10,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/success" element={<Success />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/aboutpage" element={<AboutPage />} />
         <Route path="/abstract" element={<Abstract />} />
